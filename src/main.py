1abe66d17ff3eef6ea98ae1a12be186ddb832d83
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2018-01-06 16:52:19 +0100

    [cfg] adding 'run a single test' function

--- src/main.py
+++ src/main.py
@@ -7,26 +7,31 @@ import matplotlib.pyplot as plt
 from astree.aexp import *
 from astree.bexp import *
 from astree.com import *
+from cfgraph.utils import *
 
 
 def main():
     cfg = nx.DiGraph()
 
+    cfg.add_node("START")
     for i in range(1,6):
         cfg.add_node(i)
-    cfg.add_node('_')
+    cfg.add_node("END")
 
+    cfg.add_edge("START", 1, bexp=BConstant(True), com=CSkip())
     cfg.add_edge(1, 2, bexp=BBinOp('<=', AVariable('X'), AConstant(0)), com=CSkip())
     cfg.add_edge(1, 3, bexp=BUnOp('!', BBinOp('<=', AVariable('X'), AConstant(0))), com=CSkip())
     cfg.add_edge(2, 4, bexp=BConstant(True), com=CAssign(AVariable('X'), AUnOp('-', AVariable('X'))))
     cfg.add_edge(3, 4, bexp=BConstant(True), com=CAssign(AVariable('X'), ABinOp('-', AConstant(1), AVariable('X'))))
     cfg.add_edge(4, 5, bexp=BBinOp('==', AVariable('X'), AConstant(1)), com=CSkip())
     cfg.add_edge(4, 6, bexp=BUnOp('!', BBinOp('==', AVariable('X'), AConstant(1))), com=CSkip())
-    cfg.add_edge(5, '_', bexp=BConstant(True), com=CAssign(AVariable('X'), AConstant(1)))
-    cfg.add_edge(6, '_', bexp=BConstant(True), com=CAssign(AVariable('X'), ABinOp('+', AVariable('X'), AConstant(1))))
+    cfg.add_edge(5, "END", bexp=BConstant(True), com=CAssign(AVariable('X'), AConstant(1)))
+    cfg.add_edge(6, "END", bexp=BConstant(True), com=CAssign(AVariable('X'), ABinOp('+', AVariable('X'), AConstant(1))))
 
-    nx.draw(cfg, with_labels=True, font_weight='bold')
-    plt.show()
+    print(run_test(cfg, {'X': 5}))
+
+    # nx.draw(cfg, with_labels=True, font_weight='bold')
+    # plt.show()
 
 
 if __name__ == "__main__":
