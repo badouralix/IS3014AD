5358b0850b60caa4241debb383a386f18d6db90c
Ayaz Badouraly <ayaz.badouraly@via.ecp.fr>
2018-01-08 10:49:08 +0100

    [cfg] adding type tag

--- src/main.py
+++ src/main.py
@@ -20,12 +20,12 @@ def main():
     cfg.add_node("END")
 
     cfg.add_edge("START", 1, bexp=BConstant(True), com=CSkip())
-    cfg.add_edge(1, 2, bexp=BBinOp('<=', AVariable('X'), AConstant(0)), com=CSkip())
-    cfg.add_edge(1, 3, bexp=BUnOp('!', BBinOp('<=', AVariable('X'), AConstant(0))), com=CSkip())
+    cfg.add_edge(1, 2, typename="CIF", bexp=BBinOp('<=', AVariable('X'), AConstant(0)), com=CSkip())
+    cfg.add_edge(1, 3, typename="CIF", bexp=BUnOp('!', BBinOp('<=', AVariable('X'), AConstant(0))), com=CSkip())
     cfg.add_edge(2, 4, bexp=BConstant(True), com=CAssign(AVariable('X'), AUnOp('-', AVariable('X'))))
     cfg.add_edge(3, 4, bexp=BConstant(True), com=CAssign(AVariable('X'), ABinOp('-', AConstant(1), AVariable('X'))))
-    cfg.add_edge(4, 5, bexp=BBinOp('==', AVariable('X'), AConstant(1)), com=CSkip())
-    cfg.add_edge(4, 6, bexp=BUnOp('!', BBinOp('==', AVariable('X'), AConstant(1))), com=CSkip())
+    cfg.add_edge(4, 5, typename="CIF", bexp=BBinOp('==', AVariable('X'), AConstant(1)), com=CSkip())
+    cfg.add_edge(4, 6, typename="CIF", bexp=BUnOp('!', BBinOp('==', AVariable('X'), AConstant(1))), com=CSkip())
     cfg.add_edge(5, "END", bexp=BConstant(True), com=CAssign(AVariable('X'), AConstant(1)))
     cfg.add_edge(6, "END", bexp=BConstant(True), com=CAssign(AVariable('X'), ABinOp('+', AVariable('X'), AConstant(1))))
 
