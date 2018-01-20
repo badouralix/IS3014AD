1c544e71d5e807e6764a874cb32d959f16c311ce
Danguy Des Deserts Venceslas <wenceslas.desdeserts@laposte.net>
2018-01-20 15:42:01 +0100

    Add labels to main code of parser module (bug fix)

--- src/syntax/parser.py
+++ src/syntax/parser.py
@@ -28,5 +28,5 @@ parser = yacc.yacc(debug=False, write_tables=False)
 
 if __name__ == "__main__":
     from utils.printer import print_ast
-    print_ast(parser.parse("if (x == 1) {x := 0; y:=2; z := 3;} else {x := 1;}"))
-    print_ast(parser.parse("x:= 5; while (x > 0) {x := x - 1;} print(x * 2 + 1);"))
+    print_ast(parser.parse("0: if (x == 1) {1: x := 0; 2: y:=2; 3: z := 3;} else {4: x := 1;}"))
+    print_ast(parser.parse("0: x:= 5; 1: while (x > 0) {2: x := x - 1;} 3: print(x * 2 + 1);"))
